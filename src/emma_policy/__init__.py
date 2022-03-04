a734b94495e80f722d02131296ef0f4f26bb7248
Amit Parekh <7276308+amitkparekh@users.noreply.github.com>
2022-03-04 11:50:19 +0000

    Move registration of model components to `emma_policy.__init__` (#98)

--- src/emma_policy/__init__.py
+++ src/emma_policy/__init__.py
@@ -1,1 +1,20 @@
+from transformers import AutoConfig, AutoModel, AutoModelForCausalLM, AutoTokenizer
+
+from emma_policy.models import (
+    EmmaConfig,
+    EmmaForConditionalGeneration,
+    EmmaModel,
+    EmmaTokenizer,
+    EmmaTokenizerFast,
+)
 from emma_policy.train import train_model
+
+
+AutoConfig.register("emma", EmmaConfig)
+AutoTokenizer.register(
+    EmmaConfig,  # type: ignore[arg-type]
+    slow_tokenizer_class=EmmaTokenizer,
+    fast_tokenizer_class=EmmaTokenizerFast,
+)
+AutoModel.register(EmmaConfig, EmmaModel)
+AutoModelForCausalLM.register(EmmaConfig, EmmaForConditionalGeneration)

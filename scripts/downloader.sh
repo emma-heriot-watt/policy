322ebc84195ec1041e14dcdb0d8102456d843858
Alessandro Suglia <alessandro.suglia@gmail.com>
2022-03-05 16:45:30 +0000

    Fixed permanent link to VL-T5 splits

--- scripts/downloader.sh
+++ scripts/downloader.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-wget "https://www.dropbox.com/sh/5frv7bkl5ufqxx7/AABvbh_w5dPMHAbJpEJhgqHza?dl=1" -O storage/data/splits.zip;
+wget "https://www.dropbox.com/sh/m26a281y97ok6wm/AAALB7np-NwyFGsjr8aH-YUDa?dl=1" -O storage/data/splits.zip;
 mkdir storage/data/vl-t5-splits;
 unzip storage/data/splits.zip -d storage/data/vl-t5-splits/;
 rm storage/data/splits.zip;

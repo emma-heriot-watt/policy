2bb3ad7e8c7840857f46a467fd5f7256717460d1
Alessandro Suglia <alessandro.suglia@gmail.com>
2022-10-06 11:42:40 +0100

    Bugfix: fixed exact match computation

--- src/emma_policy/utils/simbot_action_metrics.py
+++ src/emma_policy/utils/simbot_action_metrics.py
@@ -6,7 +6,11 @@ from torchmetrics import Metric
 
 
 class SimbotActionExactMatch(Metric):
-    """Loss for a exact match."""
+    """Performs exact match at the sequence length.
+
+    This means that we have a match if and only if *all* the elements of a sequence match. Masked
+    elements are ignored if an appropriate mask tensor is specified.
+    """
 
     def __init__(self, dist_sync_on_step: bool = True) -> None:
         super().__init__(dist_sync_on_step=dist_sync_on_step)
@@ -18,9 +22,13 @@ class SimbotActionExactMatch(Metric):
         self, predicted: torch.Tensor, ground_truth: torch.Tensor, mask: torch.Tensor
     ) -> None:
         """Update loss sum and number of task samples."""
+        # first compute all the matches ignoring padding
         masked_matches = (predicted == ground_truth) * mask
-        masked_matches = masked_matches.sum(-1)
-        self.correct += (masked_matches > 0).sum()
+        # then make sure that the number of matched elements is equal to the actual sequence length
+        num_matches_per_batch = masked_matches.sum(-1)
+        seq_length_per_batch = mask.sum(-1)
+        correct_matches = (num_matches_per_batch == seq_length_per_batch).int()
+        self.correct += correct_matches.sum()
         self.total += cast(torch.Tensor, masked_matches.size(0))
 
     def compute(self) -> Union[torch.Tensor, Any]:

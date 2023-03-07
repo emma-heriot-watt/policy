ec38e8fab27f3a30e16a7250474ab9ccd0bbf4b8
George Pantazopoulos <georgepantaz94@gmail.com>
2023-03-07 14:16:53 +0000

    feat: patch button in simbot action output processor (#731)

--- src/emma_policy/commands/run_simbot_action_api.py
+++ src/emma_policy/commands/run_simbot_action_api.py
@@ -152,21 +152,6 @@ async def healthcheck(response: Response) -> str:
     return "success"
 
 
-# [deprecated!]
-# @app.post("/generate_raw_text_match", status_code=status.HTTP_200_OK)
-# async def generate_raw_text_match(request: Request, response: Response) -> Optional[str]:
-#     """Endpoint for simple raw text matching."""
-#     try:
-#         simbot_request = GenerateRequest.parse_obj(await request.json())
-#     except Exception as request_err:
-#         logging.exception("Unable to parse request", exc_info=request_err)
-#         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
-#         raise request_err
-#     with tracer.start_as_current_span("Raw text match"):
-#         output_string = api_store["raw_text_matcher"](simbot_request)
-#     return output_string
-
-
 @app.post("/generate_find", status_code=status.HTTP_200_OK)
 async def generate_find(request: Request, response: Response) -> list[str]:  # noqa: WPS231
     """Endpoint for find."""
@@ -178,7 +163,7 @@ async def generate_find(request: Request, response: Response) -> list[str]:  # n
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
         raise request_err
 
-    (batch, decoder_input_ids, step_index) = api_store["input_builder"](
+    (raw_input, batch, decoder_input_ids, step_index) = api_store["input_builder"](
         simbot_request, task=Task.visual_grounding
     )
     with tracer.start_as_current_span("Model inference"):
@@ -242,7 +227,7 @@ async def grab_from_history(request: Request, response: Response) -> Optional[in
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
         raise request_err
 
-    (batch, decoder_input_ids, step_index) = api_store["input_builder"](
+    (raw_input, batch, decoder_input_ids, step_index) = api_store["input_builder"](
         simbot_request, task=Task.visual_grounding
     )
     with tracer.start_as_current_span("Model inference"):
@@ -306,7 +291,7 @@ async def generate(request: Request, response: Response) -> str:
         response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
         raise request_err
 
-    (batch, decoder_input_ids, step_index) = api_store["input_builder"](
+    (raw_input, batch, decoder_input_ids, step_index) = api_store["input_builder"](
         simbot_request, task=Task.action_execution
     )
     with tracer.start_as_current_span("Model inference"):
@@ -331,6 +316,7 @@ async def generate(request: Request, response: Response) -> str:
                     )[0]
 
                     action = api_store["output_processor"](
+                        instruction=raw_input,
                         prediction=action,
                         frame_features=simbot_request.environment_history[-1].features,
                     )
--- src/emma_policy/inference/model_wrapper/simbot_action_output_processor.py
+++ src/emma_policy/inference/model_wrapper/simbot_action_output_processor.py
@@ -6,26 +6,62 @@ from emma_common.datamodels import EmmaExtractedFeatures
 class SimBotActionPredictionProcessor:
     """Process SimBot Action predictions."""
 
-    def __call__(self, prediction: str, frame_features: list[EmmaExtractedFeatures]) -> str:
+    def __init__(self) -> None:
+        self._button_colors = ["blue", "green", "red"]
+
+    def __call__(
+        self,
+        instruction: Optional[str],
+        prediction: str,
+        frame_features: list[EmmaExtractedFeatures],
+    ) -> str:
         """Process the prediction."""
-        return self._special_robotic_arm_button_case(prediction, frame_features)
+        entity_labels = self._get_detected_objects(frame_features)
+
+        prediction_after_robot_arm = self._special_robotic_arm_button_case(
+            prediction, entity_labels
+        )
+        prediction_after_button = self._special_button_case(
+            instruction, prediction_after_robot_arm, entity_labels
+        )
+
+        return prediction_after_button
 
     def _get_detected_objects(
         self, frame_features: list[EmmaExtractedFeatures]
     ) -> Optional[list[str]]:
         """Get a list of class labels fro the detected objects."""
-        class_labels = frame_features[0].class_labels
-        if class_labels is not None:
-            class_labels = [label.lower() for label in class_labels]
-        return class_labels
+        entity_labels = frame_features[0].entity_labels
+        if entity_labels is not None:
+            entity_labels = [label.lower() for label in entity_labels]
+        return entity_labels
 
     def _special_robotic_arm_button_case(
-        self, prediction: str, frame_features: list[EmmaExtractedFeatures]
+        self, prediction: str, entity_labels: Optional[list[str]]
     ) -> str:
-        class_labels = self._get_detected_objects(frame_features)
-        if class_labels is None:
+        if entity_labels is None:
             return prediction
-        if "robot arm" in class_labels and "button" in prediction:
-            token_id = class_labels.index("robot arm") + 1
+        if "robot arm" in entity_labels and "button" in prediction:
+            token_id = entity_labels.index("robot arm") + 1
             return f"toggle robot arm <frame_token_1> <vis_token_{token_id}> <stop>."
+        return prediction
+
+    def _special_button_case(
+        self, instruction: Optional[str], prediction: str, entity_labels: Optional[list[str]]
+    ) -> str:
+        if instruction is None or entity_labels is None:
+            return prediction
+
+        for color in self._button_colors:
+            color_button = f"{color} button"
+            should_modify_prediction = (
+                color_button in entity_labels
+                and color in instruction
+                and "button" not in prediction
+            )
+
+            if should_modify_prediction:
+                token_id = entity_labels.index(color_button) + 1
+                return f"toggle button <frame_token_1> <vis_token_{token_id}> <stop>."
+
         return prediction
